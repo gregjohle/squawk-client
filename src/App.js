939b1091a6962f5842093c431745e822a4ba026c
Greg Johle <greg.johle@gmail.com>
2021-06-07 09:39:34 -0400

    adding state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [users, setUsers] = useState([
+    {
+      id: "ae8ef4f6-c795-11eb-b8bc-0242ac130003",
+      name: "Greg",
+      email: "greg@email.com",
+      password: "Password123",
+    }
+  ])
+  return <div className='App'></div>;
 }
 
 export default App;
