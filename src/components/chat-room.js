d31c9babe4cab31378b5ac7eacf0382a3ef2c308
Greg Johle <greg.johle@gmail.com>
2021-06-12 15:24:06 -0400

    added comments

--- src/components/chat-room.js
+++ src/components/chat-room.js
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 export default function Chat(props) {
   const { roomId } = props;
 
+  // these are state references for the incoming and outgoing data for the video chat
   const userVideo = useRef();
   const partnerVideo = useRef();
   const peerRef = useRef();
@@ -12,33 +13,38 @@ export default function Chat(props) {
   const otherUser = useRef();
   const userStream = useRef();
 
+  // this gives a warning because of the empty dependency array. I want this to process once when the coponent mounts.
+  // this dependency array must remain empty.
   useEffect(() => {
+    // get's the permission to use the camera and microphone
     navigator.mediaDevices
       .getUserMedia({ audio: true, video: true })
       .then((stream) => {
+        // assigns user media to user refs
         userVideo.current.srcObject = stream;
         userStream.current = stream;
-
+        // communicates user has joined room to the server, sending room ID
         socketRef.current = io.connect("/");
         socketRef.current.emit("join room", roomId);
-
+        // Sends request to server to find other users, if present.
         socketRef.current.on("other user", (userID) => {
           callUser(userID);
           otherUser.current = userID;
         });
-
+        // assigns user data to refs
         socketRef.current.on("user joined", (userID) => {
           otherUser.current = userID;
         });
-
+        // triggers offer to connect peer to peer
         socketRef.current.on("offer", handleRecieveCall);
-
+        // answers call from other user
         socketRef.current.on("answer", handleAnswer);
-
+        // handles ICE negotiation to establish Peer to Peer communication.
         socketRef.current.on("ice-candidate", handleNewICECandidate);
       });
   }, []);
 
+  // creates a peer and assigns user media to be sent
   function callUser(userID) {
     peerRef.current = createPeer(userID);
     userStream.current.getTracks().forEach((track) => {
@@ -46,6 +52,7 @@ export default function Chat(props) {
     });
   }
 
+  // creates a peer and finds a route, either STUN or TURN
   function createPeer(userID) {
     const peer = new RTCPeerConnection({
       iceServers: [
@@ -94,6 +101,7 @@ export default function Chat(props) {
     return peer;
   }
 
+  // if negotiation of route is needed, creates offer and sends media stream
   function handleNegotiationNeededEvent(userID) {
     peerRef.current
       .createOffer()
@@ -113,6 +121,8 @@ export default function Chat(props) {
       });
   }
 
+  // answers call and associates incoming media with peer
+  // sets remote description for peer
   function handleRecieveCall(incoming) {
     peerRef.current = createPeer();
     const desc = new RTCSessionDescription(incoming.sdp);
@@ -141,6 +151,7 @@ export default function Chat(props) {
       });
   }
 
+  // if answering call, sets remote description for peer
   function handleAnswer(message) {
     const desc = new RTCSessionDescription(message.sdp);
     peerRef.current.setRemoteDescription(desc).catch((e) => {
@@ -148,6 +159,7 @@ export default function Chat(props) {
     });
   }
 
+  // sends ICE candidate to peer
   function handleICECandidateEvent(e) {
     if (e.candidate) {
       const payload = {
@@ -158,6 +170,7 @@ export default function Chat(props) {
     }
   }
 
+  // receives ICE candidate
   function handleNewICECandidate(incoming) {
     const candidate = new RTCIceCandidate(incoming);
 
@@ -166,6 +179,7 @@ export default function Chat(props) {
     });
   }
 
+  // sets incoming media stream to peer for viewing
   function handleTrackEvent(e) {
     partnerVideo.current.srcObject = e.streams[0];
   }
